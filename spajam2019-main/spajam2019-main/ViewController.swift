f04cd39d00f8b0e344635e0477cda78607f2b324
masashi <swim2000616@gmail.com>
2019-06-15 01:05:12 +0900

    🎉  はじめてのコミット

---
+++ spajam2019-main/spajam2019-main/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  spajam2019-main
+//
+//  Created by Misaki Masashi on 2019/06/15.
+//  Copyright © 2019 Misaki Masashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ spajam2019-main/spajam2019-mainTests/spajam2019_mainTests.swift
@@ -0,0 +1,34 @@
+//
+//  spajam2019_mainTests.swift
+//  spajam2019-mainTests
+//
+//  Created by Misaki Masashi on 2019/06/15.
+//  Copyright © 2019 Misaki Masashi. All rights reserved.
+//
+
+import XCTest
+@testable import spajam2019_main
+
+class spajam2019_mainTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
