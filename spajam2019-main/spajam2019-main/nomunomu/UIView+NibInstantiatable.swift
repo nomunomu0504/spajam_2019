110d428155a9a1476251db0592c29a20d0ad11c9
nomunomu <black_sleepman_-.-@au.com>
2019-06-15 18:29:38 +0900

    不要なファイルをとりあえず削除

--- spajam2019-main/spajam2019-main/nomunomu/UIView+NibInstantiatable.swift
+++ spajam2019-main/spajam2019-main/nomunomu/UIView+NibInstantiatable.swift
@@ -25,29 +25,4 @@ public extension ClassNameProtocol {
 }
 
 extension NSObject: ClassNameProtocol {}
-//
-//import Foundation
-//
-//public protocol NibInstantiatable {
-//    static var nibName: String { get }
-//    static var nibBundle: Bundle { get }
-//    static var nibOwner: Any? { get }
-//    static var nibOptions: [UINib.OptionsKey: Any]? { get }
-//    static var instantiateIndex: Int { get }
-//}
-//
-//public extension NibInstantiatable where Self: NSObject {
-//    static var nibName: String { return className }
-//    static var nibBundle: Bundle { return Bundle(for: self) }
-//    static var nibOwner: Any? { return self }
-//    static var nibOptions: [UINib.OptionsKey: Any]? { return nil }
-//    static var instantiateIndex: Int { return 0 }
-//}
-//
-//public extension NibInstantiatable where Self: UIView {
-//    static func instantiate() -> Self {
-//        let nib = UINib(nibName: nibName, bundle: nibBundle)
-//        return nib.instantiate(withOwner: nibOwner, options: nibOptions).first as! Self
-//    }
-//}
 
